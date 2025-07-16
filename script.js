b81c5dc57245356215ffeb202233ca967d7ed90c
overthinker-pro <noelvabraham@gmail.com>
2025-07-16 23:48:46 +0530

    code cleanup v1

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const hamburger = document.querySelector(".hamburger-menu");
+const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navbar");
+
+window.addEventListener("DOMContentLoaded", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+});
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
